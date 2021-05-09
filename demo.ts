dfd6255e13eefb216b39b04a49ab0952bea7bf41
linux_china <libing.chen@gmail.com>
2021-05-08 22:31:52 -0700

    demo.ts

---
+++ demo.ts
@@ -0,0 +1,19 @@
+import {$, cd, pwd, question, os, fs, env} from "./mod.ts";
+import {red, yellow, blue, green} from "https://deno.land/std@0.95.0/fmt/colors.ts";
+
+// prompt to input your name
+let name = await question(blue("what's your name: "));
+console.log("Hello ", blue(name ?? "guest"));
+
+console.log("Current working directory:", pwd());
+
+// current file count
+const output = await $`ls -1 | wc -l`;
+console.log("Files count: ", parseInt(output));
+
+// your home dir
+console.log("Your home: ", os.homedir());
+
+// print your internet outbound ip
+let json = await fetch('https://httpbin.org/ip').then(resp => resp.json());
+console.log("Your ip: ", json.origin)
