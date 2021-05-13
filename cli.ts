c43dc6a8f58f65bc7e67011c27dee83e210f928e
linux_china <libing.chen@gmail.com>
2021-05-12 22:46:17 -0700

    Introduce cli function

--- cli.ts
+++ cli.ts
@@ -1,15 +1,22 @@
 import {join} from 'https://deno.land/std@0.95.0/path/mod.ts'
 
-let firstArg = Deno.args[0]
-if (['-v', '-V', '--version'].includes(firstArg)) {
-    console.log(`dx version 0.1.0`)
-    Deno.exit(0)
+async function dxCli(): Promise<void> {
+    let firstArg = Deno.args[0]
+    if (['-v', '-V', '--version'].includes(firstArg)) {
+        console.log(`dx version 0.1.0`)
+        Deno.exit(0)
+    }
+    if (typeof firstArg === 'undefined') {
+        console.log(`usage: zx <script>`)
+        Deno.exit(2)
+    } else if (firstArg.startsWith('http://') || firstArg.startsWith('https://')) {
+        await import(firstArg)
+    } else {
+        await import((join(Deno.cwd(), firstArg)))
+    }
 }
-if (typeof firstArg === 'undefined') {
-    console.log(`usage: zx <script>`)
-    Deno.exit(2)
-} else if (firstArg.startsWith('http://') || firstArg.startsWith('https://')) {
-    await import(firstArg)
-} else {
-    await import((join(Deno.cwd(), firstArg)))
+
+if (import.meta.main) {
+    dxCli();
 }
+
