316dc87815d0bd1617610abf295a7ff9bdc12e44
linux_china <libing.chen@gmail.com>
2021-05-14 17:44:21 -0700

    Add $1 and glob

--- demo.ts
+++ demo.ts
@@ -1,4 +1,4 @@
-import {$, cd, pwd, question, os, fs, env, printf} from "./mod.ts";
+import {$, cd, pwd, question, os, fs, env, printf, glob, $1} from "./mod.ts";
 import {red, yellow, blue, green} from "https://deno.land/std@0.95.0/fmt/colors.ts";
 
 // prompt to input your name
@@ -11,9 +11,19 @@ console.log("Current working directory:", pwd());
 const output = await $`ls -1 | wc -l`;
 console.log("Files count: ", parseInt(output));
 
+// output as lines
+for await (const fileName of $1`ls -1 *.ts`) {
+    console.log("TS file: ", fileName);
+}
+
 // print your internet outbound ip
 let json = await fetch('https://httpbin.org/ip').then(resp => resp.json());
 console.log("Your ip: ", json.origin)
 
 //printf
 printf("hello %s", "world");
+
+//glob *.ts
+for await (const fileName of glob("*.ts")) {
+    console.log("file: ", fileName);
+}
