0e8fdf485b9a4b9d743befa147428720489a19ed
linux_china <libing.chen@gmail.com>
2021-05-15 22:12:13 -0700

    Polishing

--- demo.ts
+++ demo.ts
@@ -1,5 +1,5 @@
 import {$, cd, pwd, question, os, fs, env, printf, glob, $a, echo} from "./mod.ts";
-import {red, yellow, blue, green} from "https://deno.land/std@0.95.0/fmt/colors.ts";
+import {red, yellow, blue, green} from "https://deno.land/std@0.96.0/fmt/colors.ts";
 
 // aliases
 $.alias("ll", "ls -al");
