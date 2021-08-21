299a34703c26a5dac3532a860bf5b7a11f417d2c
linux_china <libing.chen@gmail.com>
2021-08-21 12:58:18 -0700

    Update to std 0.105.0

--- Taskfile.ts
+++ Taskfile.ts
@@ -1,6 +1,6 @@
 /// <reference lib="esnext" />
 import {$, $o, $a, cd, pwd, question, glob, echo, cat,sleep,printf} from 'https://denopkg.com/linux-china/dx/mod.ts';
-import {red, yellow, blue, green} from "https://deno.land/std@0.96.0/fmt/colors.ts";
+import {red, yellow, blue, green} from "https://deno.land/std@0.105.0/fmt/colors.ts";
 
 export default hello;
 
